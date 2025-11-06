3fc16cf073f313bda7ca56336e4d45e47b8c26df
anna-x-li <li.anna6389@gmail.com>
2025-11-05 19:30:36 -0500

    Update src/model/constraints/aggregated_demand.jl
    Co-authored-by: Luca Bonaldo <39280783+lbonaldo@users.noreply.github.com>

--- src/model/constraints/aggregated_demand.jl
+++ src/model/constraints/aggregated_demand.jl
@@ -19,8 +19,6 @@ The functional form of the constraint is:
 "Demand Flow" in the above equation is the net balance commodity flow into the demand node `n`.
 
 """
-
-
 function add_model_constraint!(ct::AggregatedDemandConstraint, n::Node{T}, model::Model) where {T}
     ct_type = typeof(ct)
 
